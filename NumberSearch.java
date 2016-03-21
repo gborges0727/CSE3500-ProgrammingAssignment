26407cf22ba18446112aa2f0e9a4deeabaaebcf4
Gabriel Borges <gborges0727@gmail.com>
2016-03-21 16:19:49 -0400

    Add output code when no solution exists

--- NumberSearch.java
+++ NumberSearch.java
@@ -39,6 +39,9 @@ public class NumberSearch {
 		if (solution1 != null) {
 			System.out.println(solution1[0] + " and " + solution1[1] + " add to your desired number and exist in the list.");
 			System.out.println("Run time for bruteForce = " + bruteForceRunTime + " in nano-seconds or "  + (bruteForceRunTime / 1000000000.0) + " seconds");
+		} else {
+			System.out.println("No solution was detected! :(");
+			System.out.println("Run time for bruteForce = " + bruteForceRunTime + " in nano-seconds or "  + (bruteForceRunTime / 1000000000.0) + " seconds");
 		}
 
 		// Run binarySearch solution on the list
@@ -48,6 +51,9 @@ public class NumberSearch {
 		long binarySearchRunTime = binarySearchEndTime - binarySearchStartTime;
 		if (solution2 != null) {
 			System.out.println(solution2[0] + " and " + solution2[1] + " add to your desired number and exist in the list.");
+			System.out.println("Run time for binarySearch = " + binarySearchRunTime + " in nano-seconds or " + (binarySearchRunTime / 1000000000.0) + " seconds");
+		} else {
+			System.out.println("No solution was detected! :(");
 			System.out.println("Run time for binarySearch = " + binarySearchRunTime + " in nano-seconds or " + (binarySearchRunTime / 1000000000.0) + " seconds");
 		}
 	}
